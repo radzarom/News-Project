5bb6b564552112be4360cfc327a223f34d51c712
radzarom <aadamrobson@gmail.com>
2023-01-16 12:18:30 +0000

    set up app, controller, model.js files, and test file for app

---
+++ __tests__/app.test.js
@@ -0,0 +1,10 @@
+const {} = require(`${__dirname}/../db/app.js`)
+
+
+describe('app.js test suite', () => {
+    describe('GET /api/topics', () => {
+        test('should ', () => {
+            
+        });
+    });
+});
---
+++ db/app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const {} = require(`${__dirname}/controller.js`);
+
+const app = express()
+app.use(express.json());
