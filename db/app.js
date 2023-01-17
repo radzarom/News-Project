7d1ef30b7ce7268b5dd868f140cb716e0d0745f3
radzarom <aadamrobson@gmail.com>
2023-01-17 14:19:24 +0000

    completed task 5, get article by ID endpoint with 404 error handling

--- db/app.js
+++ db/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getTopics, getArticles} = require(`${__dirname}/controller.js`);
+const {getTopics, getArticles, getArticleByID} = require(`${__dirname}/controller.js`);
 
 const app = express()
 app.use(express.json());
@@ -8,10 +8,20 @@ app.get('/api/topics', getTopics);
 
 app.get('/api/articles', getArticles);
 
+app.get('/api/articles/:article_id', getArticleByID);
+
 
 app.use((request, response, next) => {
 
     response.status(404).send({msg: 'Path not found'})
+})
+
+app.use((error, request, response, next) => {
+
+    if(error.status && error.msg) {
+
+        response.status(error.status).send({msg: error.msg})
+    }
 })
 
 app.use((error, request, response, next) => {
